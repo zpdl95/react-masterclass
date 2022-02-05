070232b83c09699410da6a68a002caaa2d454a83
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-06 00:07:48 +0900

    #2.6 Super Recap

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ const Wrapper = styled.div`
   height: 500px;
 `;
 
+const Input = styled.input.attrs({ required: true, maxLength: 5 })``;
+
 const animation = keyframes`
   0%{
     transform:rotate(0deg);
@@ -18,13 +20,13 @@ const animation = keyframes`
 
 const Emoji = styled.span`
   font-size: 36px;
+  animation: ${animation} 1s linear infinite;
 `;
 
 const Box = styled.div`
   height: 100px;
   width: 100px;
   background-color: tomato;
-  animation: ${animation} 1s linear infinite;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -38,6 +40,7 @@ function App() {
     <Wrapper>
       <Box>
         <Emoji>🎇</Emoji>
+        <Input />
       </Box>
     </Wrapper>
   );
