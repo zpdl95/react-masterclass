e2e0413b73dece9419d3ca286c0b4ff27f8ee6e7
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-05 16:39:50 +0900

    #2.2 Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
-const BoxTwo = styled.div`
-  background-color: red;
-  width: 100px;
-  height: 100px;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne />
-      <BoxTwo />
+      <Box bgColor={"teal"} />
+      <Circle bgColor={"tomato"} />
     </Father>
   );
 }
