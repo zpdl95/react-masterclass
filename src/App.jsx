f8c087b5d3ad288a0726c4f31533e416255ec51e
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-05 19:07:26 +0900

    #2.4 Animations and Pseudo Selectors

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-  back
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const animation = keyframes`
+  0%{
+    transform:rotate(0deg);
+  }
+  100%{
+    transform:rotate(180deg);
+  }
+`;
+
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
   background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+  &:hover {
+    width: 50px;
+    height: 50px;
+  }
+  span {
+    font-size: 30px;
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🎇</span>
+      </Box>
+    </Wrapper>
   );
 }
 
