5d2487b0041ff2eda1d5cc5520e635e2428ab09c
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-11 23:31:46 +0900

    #5.13 Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,54 @@
-import { IToDoState } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDoState, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDoState) {
+function ToDo({ text, category, id }: IToDoState) {
+  const setToDos = useSetRecoilState(toDoState);
+
+  /* typescript에서 event의 type을 지정하는 방법 */
+  /* React.EventName<HTMLElementName> */
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = e;
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Done</button>
-      <button>Doing</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
     </li>
   );
 }
 export default ToDo;
+// function ToDo({ text, category }: IToDoState) {
+//   const onClick = (newCategory: IToDoState["category"]) => {};
+//   return (
+//     <li>
+//       <span>{text}</span>
+//       {category !== "TO_DO" && (
+//         <button onClick={() => onClick("TO_DO")}>To Do</button>
+//       )}
+//       {category !== "DONE" && (
+//         <button onClick={() => onClick("DONE")}>Done</button>
+//       )}
+//       {category !== "DOING" && (
+//         <button onClick={() => onClick("DOING")}>Doing</button>
+//       )}
+//     </li>
+//   );
+// }
+// export default ToDo;
