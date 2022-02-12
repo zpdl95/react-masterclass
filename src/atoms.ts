c757604cd8514e67751cbd42cf0e0d68bc4699f3
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-12 17:16:15 +0900

    #5.16 Selectors part One

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDoState {
   text: string;
@@ -9,4 +9,18 @@ export interface IToDoState {
 export const toDoState = atom<IToDoState[]>({
   key: "toDos",
   default: [],
+});
+
+/* selector = atom의 output을 변형시킴 */
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    /* recoilValue를 가져오는 get함수 */
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
+  },
 });
