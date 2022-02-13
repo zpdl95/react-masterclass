87d0da2fab8b0d0c6c321d90a3d9f8165e66def5
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-13 18:52:00 +0900

    #6.0 Get Selectors

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,35 +1,14 @@
 import { atom, selector } from "recoil";
 
-/* enum = 열거형으로 이름이 있는 상수들의 집합을 정의할 수 있습니다 */
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDoState {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
+export const minueState = atom({
+  key: "minute",
+  default: 0,
 });
 
-export const toDoState = atom<IToDoState[]>({
-  key: "toDos",
-  default: JSON.parse(localStorage.getItem("toDos") as string) || [],
-});
-
-/* selector = atom의 output을 변형시킴 */
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hour",
   get: ({ get }) => {
-    /* recoilValue를 가져오는 get함수 */
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minute = get(minueState);
+    return minute / 60;
   },
 });
