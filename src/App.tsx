af3aa70c3ece38fc95b967a5b759f74d1f7fab5e
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-13 19:39:21 +0900

    #6.2 Drag and Drop part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import React from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { hourSelector, minueState } from "./atoms";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
-  const [minute, setMinute] = useRecoilState(minueState);
-  const [hour, setHour] = useRecoilState(hourSelector);
-
-  const onMinuteChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setMinute(+e.currentTarget.value);
-  };
-
-  const onHourChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setHour(+e.currentTarget.value);
-  };
+  const onDragEnd = () => {};
   return (
-    <>
-      <input
-        value={minute}
-        onChange={onMinuteChange}
-        type="number"
-        placeholder="Minutes"
-      />
-      <input
-        value={hour}
-        onChange={onHourChange}
-        type="number"
-        placeholder="Hours"
-      />
-    </>
+    /* onDragEnd = 드래그가 끝났을때 실행되는 함수 */
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        {/* Droppable = children으로 함수를 받는다 */}
+        {/* id도 받아야함 */}
+        <Droppable droppableId="one">
+          {() => (
+            <ul>
+              {/* Draggable = children으로 함수를 받는다 */}
+              {/* id와 index를 받아야함 */}
+              <Draggable draggableId="first" index={0}>
+                {() => <li>one</li>}
+              </Draggable>
+              <Draggable draggableId="second" index={1}>
+                {() => <li>two</li>}
+              </Draggable>
+              <Draggable draggableId="third" index={2}>
+                {() => <li>three</li>}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
 }
 
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,19 +1,1 @@
 import { atom, selector } from "recoil";
-
-export const minueState = atom({
-  key: "minute",
-  default: 0,
-});
-
-/* selector = 마치 중계기 같은 역할을 한다 */
-export const hourSelector = selector<number>({
-  key: "hour",
-  get: ({ get }) => {
-    const minute = get(minueState);
-    return minute / 60;
-  },
-  set: ({ set }, newValue) => {
-    const minute = Number(newValue) * 60;
-    set(minueState, minute);
-  },
-});
