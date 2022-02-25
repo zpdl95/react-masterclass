ce0da4f52a6c07b9226fd665947856df779b9982
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-26 00:29:00 +0900

    #7.4 Variants part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -13,15 +13,52 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 20%;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  place-items: center;
+`;
+
+const Circle = styled(motion.div)`
+  width: 75px;
+  height: 75px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 50%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-/* 애니메이션 구성을 오브젝트로 만듬 */
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", mass: 5 } },
+/* Variants = motion패키지에 있는 Variant type  */
+const boxVariants: Variants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      /* delayChildren = 자식컴포넌트에 애니메이션 딜레이를 준다 */
+      delayChildren: 0.5,
+      /* staggerChildren = 각각의 자식컴포넌트에 순차적인 애니메이션 딜레이를 준다 */
+      staggerChildren: 0.08,
+    },
+  },
+};
+
+const circleVarians: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App() {
@@ -30,7 +67,14 @@ function App() {
       {/* variants에 애니메이션 구성 오브젝트를 넣음,
       initial에 구성 오브젝트의 key값을 넣음,
       animate에 구성 오브젝트의 key값을 넣음 */}
-      <Box variants={myVars} initial="start" animate="end" />
+      {/* 부모컴포넌트에 initial과 animate가 있으면 자동으로
+      자식컴포넌트에도 initial과 animate prop이 들어간다 */}
+      <Box variants={boxVariants} initial={"start"} animate={"end"}>
+        <Circle variants={circleVarians}></Circle>
+        <Circle variants={circleVarians}></Circle>
+        <Circle variants={circleVarians}></Circle>
+        <Circle variants={circleVarians}></Circle>
+      </Box>
     </Wrapper>
   );
 }
