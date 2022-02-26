62c15ce0929cdb92eda15d8aebde31e9f0246e82
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-27 01:03:16 +0900

    #7.10 SVG Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,54 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-  Variants,
-} from "framer-motion";
-import { useEffect } from "react";
+import { motion, Variants } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  height: 300vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: ;
+  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
-/* 애니메이션 컴포넌트를 스타일드컴포넌트 함수에 넣어서 사용 */
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 20%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled(motion.svg)`
+  width: 300px;
+  height: 300px;
+  path {
+    /* stroke = 선(line) */
+    stroke: white;
+    stroke-width: 2;
+  }
 `;
 
-/* Variants = motion패키지에 있는 Variant type  */
-const boxVariants: Variants = {
-  hover: { scale: 1, rotate: 90 },
-  click: { scale: 1, borderRadius: "50%" },
+const svgVariant: Variants = {
+  start: {
+    pathOffset: 0,
+    pathLength: 0,
+    fill: "rgba(255,255,255,0)",
+  },
+  end: {
+    pathOffset: 1,
+    pathLength: 1,
+    fill: "rgba(255,255,255,1)",
+    transition: {
+      duration: 3,
+      /* transition안에서 'fill'처럼 따로 prop을 정해서 설정해주면 따로 적용됨 */
+      fill: { duration: 2, delay: 1 },
+    },
+  },
 };
 
 function App() {
-  /* useMotionValue = React의 rerendering에 영향을 주지 않는 값 */
-  const x = useMotionValue(0);
-  const gradient = useTransform(
-    x,
-    [-800, 0, 800],
-    [
-      "linear-gradient(135deg, rgb(238, 0, 0), rgb(238, 0, 153))",
-      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
-      "linear-gradient(135deg, rgb(221, 0, 238), rgb(143, 0, 238))",
-    ]
-  );
-  /* useViewportScroll = 페이지에서의 스크롤링 값 */
-  const { scrollYProgress } = useViewportScroll();
-  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.1, 1, 2]);
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <motion.path
+          variants={svgVariant}
+          initial={"start"}
+          animate={"end"}
+          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        />
+      </Svg>
     </Wrapper>
   );
 }
