4986906b11bf7038b7b74acf411668b53bdcecf5
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-26 18:06:09 +0900

    #7.5 Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -13,68 +13,33 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 20%;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  place-items: center;
-`;
-
-const Circle = styled(motion.div)`
-  width: 75px;
-  height: 75px;
-  background-color: white;
-  border-radius: 50%;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 /* Variants = motion패키지에 있는 Variant type  */
 const boxVariants: Variants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      /* delayChildren = 자식컴포넌트에 애니메이션 딜레이를 준다 */
-      delayChildren: 0.5,
-      /* staggerChildren = 각각의 자식컴포넌트에 순차적인 애니메이션 딜레이를 준다 */
-      staggerChildren: 0.08,
-    },
-  },
-};
-
-const circleVarians: Variants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
+  hover: { scale: 1.5, rotate: 90 },
+  click: { scale: 1, borderRadius: "50%" },
+  drag: {
+    /* drag 애니메이션 컬러를 string이 아니라 number로 주면 색변화도 애니메이션이 적용된다 */
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    transition: { duration: 5 },
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      {/* variants에 애니메이션 구성 오브젝트를 넣음,
-      initial에 구성 오브젝트의 key값을 넣음,
-      animate에 구성 오브젝트의 key값을 넣음 */}
-      {/* 부모컴포넌트에 initial과 animate가 있으면 자동으로
-      자식컴포넌트에도 initial과 animate prop이 들어간다 */}
-      <Box variants={boxVariants} initial={"start"} animate={"end"}>
-        <Circle variants={circleVarians}></Circle>
-        <Circle variants={circleVarians}></Circle>
-        <Circle variants={circleVarians}></Circle>
-        <Circle variants={circleVarians}></Circle>
-      </Box>
+      <Box
+        /* drag = 컴포넌트에 드래그가능 적용 */
+        drag
+        variants={boxVariants}
+        whileHover={"hover"}
+        whileTap={"click"}
+        whileDrag={"drag"}
+      />
     </Wrapper>
   );
 }
