db464c535b409087ba6e7b13c813c70fdffeaa20
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-27 15:05:45 +0900

    #7.13 Slider part Two

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,12 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants: Variants = {
-  invisible: {
-    x: 500,
+  entry: (back: boolean) => ({
+    x: back ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
@@ -40,29 +40,43 @@ const boxVariants: Variants = {
       duration: 0.5,
     },
   },
-  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 0.5 } },
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: { duration: 0.5 },
+  }),
 };
 
 function App() {
   const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  const prevPlease = () => setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
-            <Box
-              variants={boxVariants}
-              initial="invisible"
-              animate="visible"
-              exit="exit"
-              key={i}
-            >
-              {i}
-            </Box>
-          ) : null
-        )}
+      {/* AnimatePresence에도 custom을 넣어줘야 바로바로 동작함 */}
+      {/* exitBeforeEnter = exit애니메이션이 끝나면 다음 컴포넌트가 생성됨. 단점 느려보임 */}
+      <AnimatePresence exitBeforeEnter custom={back}>
+        {
+          <Box
+            /* custom = variants에 변수를 넘겨줌 */
+            custom={back}
+            variants={boxVariants}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            key={visible}
+          >
+            {visible}
+          </Box>
+        }
       </AnimatePresence>
       <button onClick={nextPlease}>next</button>
       <button onClick={prevPlease}>prev</button>
