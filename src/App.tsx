5aa2889368544a78e5a68baa7fc12e70a666691d
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-02-27 16:29:11 +0900

    #7.15 Final Project part One

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,19 @@ const Wrapper = styled(motion.div)`
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
+const Grid = styled.div`
+  width: 50vw;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
 const Box = styled(motion.div)`
-  width: 300px;
-  height: 300px;
-  top: 100px;
+  height: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.3);
   background-color: white;
   border-radius: 20px;
@@ -25,31 +34,49 @@ const Box = styled(motion.div)`
   align-items: center;
 `;
 
-const Circle = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  background-color: teal;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  div {
+    width: 50%;
+    height: 30%;
+  }
 `;
+
+const overlayVariants: Variants = {
+  entry: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  showing: { backgroundColor: "rgba(0, 0, 0, .6)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
 
 function App() {
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const toggle = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      {/* layoutId = 서로다른 컴포넌트를 애니메이션 동기화 시킴 */}
-      <Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
-      <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
-      <Box
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        {/* layout = 컴포넌트의 layout이 변할때 애니메이션을 생성 */}
-        <Circle layout />
-      </Box>
+    /* layoutId를 이용한 멋진 layout애니메이션 */
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="good" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked && (
+          <Overlay
+            variants={overlayVariants}
+            initial="entry"
+            animate="showing"
+            exit="exit"
+          >
+            <Box layoutId="good" />
+          </Overlay>
+        )}
+      </AnimatePresence>
     </Wrapper>
   );
 }
