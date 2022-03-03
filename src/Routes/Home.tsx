9cb96aa5f7ddec440d8d17bcb144575cd187947f
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-03-03 20:10:10 +0900

    #8.7 Slider part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,3 +1,5 @@
+import { motion, AnimatePresence, Variants } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
@@ -40,15 +42,45 @@ const Overview = styled.p`
   width: 35%;
 `;
 
+const Slider = styled(motion.div)`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+`;
+
+const rowVariants: Variants = {
+  hidden: { x: window.innerWidth },
+  visible: { x: 0, transition: { type: "tween" } },
+  exit: {
+    x: -window.innerWidth,
+    transition: { type: "tween" },
+  },
+};
+
 function Home() {
   const { data: nowPlayingData, isLoading: nowPlayingIsLoading } =
     useQuery<IGetMoviesResult>(["movies", "nowPlaying"], getMovies);
+  const [index, setIndex] = useState(0);
+
   return (
     <Wrapper>
       {nowPlayingIsLoading && <Loader>Loading...</Loader>}
       {!nowPlayingIsLoading && (
         <>
           <Banner
+            onClick={() => setIndex((prev) => prev + 1)}
             bgPhoto={makeImagePath(
               nowPlayingData?.results[0].backdrop_path || ""
             )}
@@ -56,6 +88,21 @@ function Home() {
             <Title>{nowPlayingData?.results[0].title}</Title>
             <Overview>{nowPlayingData?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                key={index}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
